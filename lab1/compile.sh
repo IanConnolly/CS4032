641492534c0414c134f5050bc5c659f1f3044c37
Ian Connolly <iconnolly@mozilla.com>
2014-10-13 17:30:40 +0100

    Perms

--- lab1/compile.sh
+++ lab1/compile.sh
@@ -2,3 +2,4 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ghc "$DIR/client.hs"
 find $DIR -name "*.o" -o -name "*.hi" | xargs rm
+chmod +x "$DIR/client"
--- lab2/compile.sh
+++ lab2/compile.sh
@@ -3,3 +3,5 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ghc "$DIR/server.hs" -XBangPatterns
 ghc "$DIR/client.hs"
 find $DIR -name "*.o" -o -name "*.hi" | xargs rm
+chmod +x "$DIR/server"
+chmod +x "$DIR/client"
