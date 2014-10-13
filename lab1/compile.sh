3613e6e043a5a08284e1a963195fee23e7dee45d
Ian Connolly <iconnolly@mozilla.com>
2014-10-13 17:29:26 +0100

    More scripts

--- lab1/compile.sh
+++ lab1/compile.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ghc "$DIR/client.hs"
 find $DIR -name "*.o" -o -name "*.hi" | xargs rm
--- lab2/compile.sh
+++ lab2/compile.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ghc "$DIR/server.hs" -XBangPatterns
 ghc "$DIR/client.hs"
